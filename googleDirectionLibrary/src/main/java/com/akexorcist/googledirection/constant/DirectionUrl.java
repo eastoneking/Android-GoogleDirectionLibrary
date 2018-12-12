534347a1ff802fa4a20bbeea270267c1b206a971
临碣 <wangds2016@163.com>
2018-12-12 17:44:39 +0800

    2

--- googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
+++ googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
@@ -23,6 +23,6 @@ package com.akexorcist.googledirection.constant;
  */
 public class DirectionUrl {
     public static String MAPS_API_URL = "https://maps.googleapis.com/maps/api/";
-    public static final String DIRECTION_API_URL = "directions/json";
+    public static String DIRECTION_API_URL = "directions/json";
 
 }
